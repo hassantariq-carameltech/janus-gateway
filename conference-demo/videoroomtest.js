6a0a3350f108e01c6000543609303b445c4395e2
Hassan Tariq <hassan.tariq@fizz.io>
2020-03-31 11:54:08 +0500

    change prod variable

--- conference-demo/videoroomtest.js
+++ conference-demo/videoroomtest.js
@@ -47,7 +47,7 @@ if(window.location.protocol === 'http:')
 	server = "http://" + window.location.hostname + ":8088/janus";
 else
 	server = "https://" + window.location.hostname + ":8089/janus";
-server = "http://conference-demo.fizz.io:8088/janus"
+// server = "http://conference-demo.fizz.io:8088/janus"
 
 var janus = null;
 var sfutest = null;
