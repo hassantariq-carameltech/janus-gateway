de5b8fbaa0891507f032964c82b074bc8ec04b02
Faizan Khan <faizan.khan@fizz.io>
2020-04-13 13:05:07 +0500

    [feature/faizan/chat] chat show/hide in slideable panel

---
+++ conference-demo/fizzWebChat.js
@@ -0,0 +1,16 @@
+var mainMargin = 0;
+
+function showChatWindow() {
+  const mainStyle = document.getElementById('main').style;
+  mainMargin = mainStyle.marginLeft;
+
+  mainStyle.marginLeft = '320px';
+  document.getElementById('chatButton').style.width = '0';
+  document.getElementById('mySidebar').style.width = '320px';
+}
+
+function hideChatWindow() {
+  document.getElementById('mySidebar').style.width = '0';
+  document.getElementById('chatButton').style.width = '80px';
+  document.getElementById('main').style.marginLeft = mainMargin;
+}
